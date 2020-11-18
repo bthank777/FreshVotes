25cb35992baa4097a89639ba086e4d550e1b127d
bthank777 <bthanktraining@gmail.com>
2020-11-18 13:16:35 -0500

    creating our first controller

---
+++ FreshVotes/src/main/java/com/freshvotes/DashboardController.java
@@ -0,0 +1,19 @@
+package com.freshvotes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashboardController {
+
+	//listen for a GET request at the root url "/" which for us is localhost:8080
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String rootView() {
+		// then return the view called "index" which is an html file
+		return "index";   // Spring knows to prepend and append to build the full address /src/main/resources/index.html
+	}
+	
+	
+	
+}
